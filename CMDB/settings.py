c7be3f9801b24a9f238e8c58543b5bb3ace3fb91
EnesBrt <enes.barut@gmx.com>
2025-01-03 16:51:30 +0100

    Add django project file and app file

--- CMDB/settings.py
+++ CMDB/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-9_@o8+=^dqs2x$boa__ys7h9kolfic!w*sy6d2s&b&vwos1-u_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "CMDB.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "cmdb",
+        "USER": "enesbarut",
+        "PASSWORD": "barut_admin",
+        "HOST": "localhost",  # or your database host
+        "PORT": "5432",  # default PostgreSQL port
     }
 }
 
