0cf0d307a2079c0c532023615953f98629f629f5
EnesBrt <enes.barut@gmx.com>
2025-01-06 14:20:40 +0100

    Test network.py

--- network.py
+++ network.py
@@ -1,12 +1,20 @@
 from netmiko import ConnectHandler
 
 # Just pick an 'invalid' device_type
-cisco1 = {
-    "device_type": "invalid",
-    "host": "cisco1.lasthop.io",
-    "username": "pyclass",
-    "password": "invalid",
+cisco = {
+    "device_type": "cisco_xe",
+    "host": "devnetsandboxiosxe.cisco.com",
+    "username": "admin",
+    "password": "C1sco12345",
+    "port": 22,
 }
 
-net_connect = ConnectHandler(**cisco1)
-net_connect.disconnect()
+net_connect = ConnectHandler(**cisco)
+# Vérification de l'uptime
+version_output = net_connect.send_command("show version")
+if "uptime is" in version_output:
+    uptime = version_output.split("uptime is")[1].split(",")[0]
+    print(f"Le routeur est actif depuis : {uptime}")
+else:
+    print("Impossible de déterminer l'uptime")
+print(version_output)
