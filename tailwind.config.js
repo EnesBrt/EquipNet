4c6904b56eab070fd46af2cc6fbc42eb93e87f54
EnesBrt <enes.barut@gmx.com>
2025-01-15 19:27:27 +0100

    Add changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./cmdb_app/**/*.{html,js}",
+    './cmdb_app/**/*.html',
+    './cmdb_app/**/*.css',
+    './cmdb_app/**/*.js',
   ],
-  
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ]
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
