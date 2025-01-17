689a037ab3a73111357f6c899170ea71da9d0565
EnesBrt <enes.barut@gmx.com>
2025-01-17 11:58:01 +0100

    Add changes

--- cmdb_app/management/commands/network.py
+++ cmdb_app/management/commands/network.py
@@ -1,43 +1,28 @@
 from netmiko import ConnectHandler
 from cmdb_app.models import NetworkEquipment
 from django.core.management.base import BaseCommand
-import signal
-import time
-import sys
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        help = "Runs a command to check network equipment status indefinitely"
 
-        def signal_handler(sig, frame):
-            self.stdout.write(self.style.SUCCESS("Exiting gracefully"))
-            sys.exit(0)
+        for equipment in NetworkEquipment.objects.all():
+            device = {
+                "device_type": equipment.device_type,
+                "host": equipment.host,
+                "username": equipment.username,
+                "password": equipment.password,
+                "port": equipment.port,
+            }
 
-            signal.signal(signal.SIGINT, signal_handler)
+            net_connect = ConnectHandler(**device)
 
-            self.stdout.write("Press Ctrl+C to stop the program")
+            version_output = net_connect.send_command("show version")
+            if "uptime is" in version_output:
+                equipment.status = "Active"
+            else:
+                equipment.status = "Inactive"
+            equipment.save()
 
-            while True:
-
-                for equipment in NetworkEquipment.objects.all():
-                    device = {
-                        "device_type": equipment.device_type,
-                        "host": equipment.host,
-                        "username": equipment.username,
-                        "password": equipment.password,
-                        "port": equipment.port,
-                    }
-
-                    net_connect = ConnectHandler(**device)
-
-                    version_output = net_connect.send_command("show version")
-                    if "uptime is" in version_output:
-                        equipment.status = "Active"
-                    else:
-                        equipment.status = "Inactive"
-                    equipment.save()
-
-                self.stdout.write("Program is running...")
-                time.sleep(60)
+        print("The program is running...")
